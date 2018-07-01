ac52eb18c1275ce102ec329ca3e7dfc3cd26e78d
k-tremen <32711403+k-tremen@users.noreply.github.com>
2018-07-01 15:18:41 +0200

    Add files via upload

---
+++ pres.php
@@ -0,0 +1,73 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>TP_ENSG</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+		<link rel="stylesheet" type"text/css" href="style2.css">
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+
+	</head>
+	<body>
+
+	<div><img src="ensg.JPG" style="width: 100%"></div>
+
+	<nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
+	<div class="container-fluid">
+		<a class="navbar-brand" href="#"><img src="logo.png"></a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarResponsive">
+			<ul class="navbar-nav ml-auto">
+			<li class="nav-item">
+				<a class="nav-link" href="#">Home</a>
+			</li>
+			<li class="nav-item active">
+				<a class="nav-link" href="#">Présentation</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#">Données</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#">Recherche</a>
+			</li>
+			</ul>
+		</div>
+	</div>
+	</nav>
+
+	<div class="container-fluid padding">
+		<div class="row welcome text-center">
+			<div class="col-12">
+				<h1 class="display-4">Présentation du TP</h1>
+			</div>
+			<hr>
+			<div class="col-12">
+				<p class="lead">L'objectif ici était de créer deux pages web. L'une d'entre elles permettant d'afficher l'ensemble de la base de données et l'autre d'effectuer une recherche basée sur le champ "nom" ou "prenom".</p>
+			</div>
+		</div>
+	</div>
+
+	<div class="container-fluid padding">
+		<div class="row text-center padding">
+			<div class="col-xs-12 col-sm-6 col-md-4">
+				<img src="html5.png">
+				<h3>HTML5</h3>
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-4">
+				<img src="bootstrap.png">
+				<h3>BOOTSTRAP</h3>
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-4">
+				<img src="css3.png">
+				<h3>CSS3</h3>
+			</div>
+		</div>
+		<hr class="my-4">
+	</div>
+	</body>
+</html>
